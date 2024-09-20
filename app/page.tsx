b25aee17c8a2f934438f8f288bb4301a3b48b1e3
Jose Sanchez <engineer@copydataai.com>
2024-09-20 23:53:08 +0100

    feat(pages): add main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,16 +1,18 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
+import React from "react";
+import Hero from "../components/Hero";
+import { NavBar } from "../components/NavBar";
+import Features from "../components/Features";
+import Footer from "../components/Footer";
 
-export default async function Index() {
+const Home: React.FC = () => {
   return (
     <>
+      <NavBar />
       <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
-      </main>
+      <Features />
+      <Footer />
     </>
   );
-}
+};
+
+export default Home;
