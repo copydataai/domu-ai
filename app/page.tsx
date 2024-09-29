4927e4759c72bbb94be99ac5533be0ce3f20ab62
Jose Sanchez <engineer@copydataai.com>
2024-09-29 10:37:51 +0100

    feat(pages): add logo-carousel, genai-agents

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,20 @@
 import React from "react";
-import Hero from "../components/Hero";
-import { NavBar } from "../components/NavBar";
-import Features from "../components/Features";
-import Footer from "../components/Footer";
+import Hero from "@/components/Hero";
+import { NavBar } from "@/components/NavBar";
+import LogoCarousel from "@/components/logo-carousel";
+import GenAIAgents from "@/components/genai-agents";
+import SuccessSection from "@/components/success-section";
+import Features from "@/components/Features";
+import Footer from "@/components/Footer";
 
 const Home: React.FC = () => {
   return (
     <>
       <NavBar />
       <Hero />
-      <Features />
+      <LogoCarousel />
+      <GenAIAgents />
+      <SuccessSection />
       <Footer />
     </>
   );
